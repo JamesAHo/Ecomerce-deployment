98cc8167a7e39ba3621e95f49c4be1a23eaa1277
James Anh <james.anh184@gmail.com>
2023-05-23 19:27:29 -0700

    added provider and additional resources

--- main.tf
+++ main.tf
@@ -18,3 +18,35 @@ resource "aws_instance" "ecommerce1" {
     ambiente = "bootcamp"
   }
 }
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP traffic on EC2 instance"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH to EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP to EC2"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
