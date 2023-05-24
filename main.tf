4158930e6fd9762882b048c60d88b717a6856b4b
James Anh <james.anh184@gmail.com>
2023-05-23 19:26:13 -0700

    added  resources

--- main.tf
+++ main.tf
@@ -5,3 +5,16 @@ variable "vpc_id" {
 variable "key_name" {
   default = "sshkey1"
 }
+# resources
+resource "aws_instance" "ecommerce1" {
+  ami = "ami-032930428bf1abbff"
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  associate_public_ip_address = true
+
+  #tag
+  tags = {
+    Name = "ecommerce1"
+    ambiente = "bootcamp"
+  }
+}
